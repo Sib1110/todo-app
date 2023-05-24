6bde991d9fd0fc547771178c83af93a398802d54
Sib1110 <powhonor@gmail.com>
2023-05-25 00:06:41 +0900

    add fonts and make form

--- src/components.tsx/TodoForm.tsx
+++ src/components.tsx/TodoForm.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function TodoForm() {
-  return <div>TOdoForm</div>;
+  return (
+    <div>
+      <form>
+        <label htmlFor="userInput">목록 작성</label>
+        <input type="text" id="userInput" />
+      </form>
+    </div>
+  );
 }
 
 export default TodoForm;
